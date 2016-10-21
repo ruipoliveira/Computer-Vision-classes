5edc1f554269321487e016c79e55fcebf9e12534
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-10-21 11:11:47 +0100

    update exerc aula5.

--- aula5/fileSource.h
+++ aula5/fileSource.h
@@ -4,8 +4,8 @@
 #include <iostream>
 
 int ex1();
-//int ex2();
-//int ex3();
-//int ex4();
+int ex1_2();
+int ex3();
+int ex4();
 int FindAndDisplayChessboard(cv::Mat image,int board_w,int board_h, std::vector<cv::Point2f> *corners);
 
