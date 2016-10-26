3b091908d6ab96d1bd7d2b47212040449d1712fe
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-10-26 10:26:35 +0100

    init aula 6

---
+++ aula6/fileSource.h
@@ -0,0 +1,9 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+int ex1();
+int ex2();
+int ex3();
+int ex4();
