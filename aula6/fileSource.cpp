a68c414c16b14642658a70296f637d231d04363e
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-10-27 15:05:26 +0100

    add ex1 e ex2 aula 6

--- aula6/fileSource.cpp
+++ aula6/fileSource.cpp
@@ -9,7 +9,6 @@
 #include <string>
 #include <unistd.h>
 
-
 using namespace cv;
 using namespace std;
 
@@ -90,11 +89,148 @@ int ex1(){
             sucesses++;
         }
     }
+
+
+
     return 0;
 }
 
 
 int ex2(){
+
+  // ChessBoard Properties
+    int n_boards = 13; //Number of images
+    int board_w = 9;
+    int board_h = 6;
+
+    int board_sz = board_w * board_h;
+
+    char filename[200];
+
+    // Chessboard coordinates and image pixels
+    std::vector<std::vector<cv::Point3f> > object_points;
+    std::vector<std::vector<cv::Point2f> > image_pointsL, image_pointsR ;
+
+    // Corners detected in each image
+    std::vector<cv::Point2f> cornersR, cornersL;
+
+    int corner_countL, corner_countR;
+
+    cv::Mat imageL, imageR;
+    int i;
+
+    int sucesses = 0;
+
+    // chessboard coordinates
+    std::vector<cv::Point3f> obj;
+    for(int j=0;j<board_sz;j++)
+    obj.push_back(cv::Point3f(float(j/board_w), float(j%board_w), 0.0));
+
+
+
+    for (i=0;i<n_boards;i++){
+        // read image
+        sprintf(filename,"../images/stereovision/left%02d.jpg",i+1);
+        printf("\nReading %s",filename);
+        imageR = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+
+
+        if(!imageR.data){
+            printf("\nCould not load image file: %s\n",filename);
+            getchar();
+            return 0;
+        }
+
+        // find and display corners
+        corner_countR = FindAndDisplayChessboard(imageR,board_w,board_h,&cornersR);
+
+
+        sprintf(filename,"../images/stereovision/right%02d.jpg",i+1);
+        printf("\nReading %s",filename);
+        imageL = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+
+
+        if(!imageL.data){
+            printf("\nCould not load image file: %s\n",filename);
+            getchar();
+            return 0;
+        }
+
+
+        corner_countL = FindAndDisplayChessboard(imageL,board_w,board_h,&cornersL);
+
+        if (corner_countL == board_w * board_h){
+            image_pointsL.push_back(cornersL);
+            object_points.push_back(obj);
+            sucesses++;
+        }
+
+        if (corner_countR == board_w * board_h){
+            image_pointsR.push_back(cornersR);
+            //object_pointsR.push_back(obj);
+            sucesses++;
+        }
+
+    }
+
+
+    Mat cameraMatrix1, distCoeffs1;
+    Mat cameraMatrix2, distCoeffs2;
+
+    Mat R, T, E, F;
+
+    double rms = stereoCalibrate(object_points, image_pointsL, image_pointsR,
+            cameraMatrix1, distCoeffs1,
+            cameraMatrix2, distCoeffs2,
+            imageL.size(), R, T, E, F,
+            TermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 50, 1e-6),
+            CV_CALIB_FIX_FOCAL_LENGTH );
+
+
+    cout <<  endl <<"done with RMS error=" << rms << endl;
+
+    cv::FileStorage fs("stereoParams.xml", cv::FileStorage::WRITE);
+
+    if (!fs.isOpened()){
+        std::cout << "Failed to open stereoParams.xml" << std::endl;
+        return 1;
+    }
+    else
+        std::cout << "Writing camera parameters" << std::endl;
+        fs << "cameraMatrix_1" << cameraMatrix1;
+        fs << "distCoeffs_1" << distCoeffs1;
+        fs << "cameraMatrix_2" << cameraMatrix2;
+        fs << "distCoeffs_2" << distCoeffs2;
+        fs << "rotation" << R;
+        fs << "translation" << T;
+        fs << "essential" << E;
+        fs << "fundamental" << F;
+        fs.release();
+
+
+
+    Mat imageUndistortedL;
+    for (i=0;i<1;i++){
+        // read image
+        sprintf(filename,"../images/stereovision/left%02d.jpg",i+1);
+        printf("\nReading %s",filename);
+        imageL = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+
+
+        //imshow("dsad", imageL);
+
+        undistort(imageL, imageUndistortedL, cameraMatrix1, distCoeffs1);
+
+
+
+    }
+    std::cout << cameraMatrix1 << std::endl;
+
+    imshow("sdada", imageL);
+    imshow("undistorted", imageUndistortedL);
+
+    cv::waitKey(-1);
+
 
     return 0;
 }
