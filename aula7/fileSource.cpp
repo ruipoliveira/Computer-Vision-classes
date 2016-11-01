670e819f6096eff67a05e605e2c8707323394426
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-11-01 18:15:13 +0000

    init aula 7

---
+++ aula7/fileSource.cpp
@@ -0,0 +1,21 @@
+#include "fileSource.h"
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+#include <unistd.h>
+
+using namespace cv;
+using namespace std;
+
+
+int ex1(){
+
+
+    return 0;
+}
+
