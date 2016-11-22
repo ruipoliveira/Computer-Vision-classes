65c526271cfead963ecf0eabd73d68addfffbd33
toomyy94 <tomasrodrigues@ua.pt>
2016-11-22 15:14:31 +0000

    exs aula 9, miss report and bug fixes

--- aula9/main.cpp
+++ aula9/main.cpp
@@ -1,4 +1,4 @@
-#include "fileSource.h"
+//#include "fileSource.h"
 #include <iostream>
 #include <vector>
 #include <stdio.h>
@@ -10,15 +10,39 @@
 #include <unistd.h>
 
 
+//opencv
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/background_segm.hpp>
+//C
+//#include <stdio.h>
+//C++
+#include <iostream>
+#include <sstream>
+
+#include "fileSource.h"
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+#include <unistd.h>
+
+using namespace cv;
+using namespace std;
+
+
 int main(int argc, char **argv){
     int val_return;
 
     //val_return = ex1();
     //val_return = ex2();
     val_return = ex3();
+
     return val_return;
 
 }
-
-
 
