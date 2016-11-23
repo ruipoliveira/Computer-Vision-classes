94b4157d763aa6d35da04c6ca51e559a8fb25ecc
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-11-23 11:21:38 +0000

    add project aula10

---
+++ aula10/fileSource.cpp
@@ -0,0 +1,27 @@
+#include "fileSource.h"
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+#include <unistd.h>
+
+using namespace cv;
+using namespace std;
+
+
+
+int ex1(){
+
+
+    return 0;
+}
+
+
+int ex2(){
+
+    return 0;
+}
---
+++ aula10/fileSource.h
@@ -0,0 +1,7 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+int ex1();
+int ex2();
